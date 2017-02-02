82745a867db707451da77d5a61d56de004885cc3
Alex Applegate <alexapplegate@alexs-macbook-pro.local>
2017-02-02 12:04:57 -0500

    Initial Commit

---
+++ Team Survey/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Team Survey
+//
+//  Created by Alex Applegate on 2/2/17.
+//  Copyright © 2017 Applesnap INC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
